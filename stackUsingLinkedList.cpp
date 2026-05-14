e76687be0b3f5754af677c8abbc3765c7e9ba553
hermayani2786 <herma.yani2786@gmail.com>
2026-05-14 11:06:17 +0700

    menambahkan validasi pilihan menu dan fitur keluar program

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -118,3 +118,18 @@ int main()
                 cout << "Stack is empty.No top value." << endl;
             }
             break;
+
+        case 4:
+            cout << "Exiting program." << endl;
+            break;
+
+        default:
+            cout << "invalid choice. Try again." << endl;
+            break;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
